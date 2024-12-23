b8b35b34e4e2f94eea012199fa10341fc55ae880
Ahmed Hany <ah7608867@gmail.com>
2024-12-23 06:04:54 +0200

    connect to postgresql with orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 
+import {
+  TypeOrmModule
+} from '@nestjs/typeorm';
+
+
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [UserModule, PostModule, AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true, // This will automatically create database tables that don't exist, useful for development
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'new_password',
+        database: "nest-learn"
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
