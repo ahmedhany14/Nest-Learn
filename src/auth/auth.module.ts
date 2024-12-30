79cdfa9bde0d889ac174d36a261c308a7f96d946
Ahmed Hany <ah7608867@gmail.com>
2024-12-30 16:37:09 +0200

    generating the token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,33 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './service/auth.service';
 
+// Modules
 import { UserModule } from '../user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+// Services
 import { BcryptProvider } from './service/bcrypt.provider';
 import { Hashing } from './service/hashing.provider';
+import { AuthService } from './service/auth.service';
+
+// configurations
+import jwtConfig from './config/jwt.config';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, {
-    provide: Hashing,
-    useClass: BcryptProvider,
-  }],
+  providers: [
+    AuthService,
+    {
+      provide: Hashing,
+      useClass: BcryptProvider,
+    },
+  ],
   exports: [AuthService, Hashing],
-  imports: [forwardRef(() => UserModule)],
+  imports: [
+    forwardRef(() => UserModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -3,25 +3,30 @@ import { Inject, Injectable, forwardRef } from '@nestjs/common';
 // Services
 import { UserService } from '../../user/services/user.service';
 import { Hashing } from './hashing.provider';
-
+import { JwtService } from '@nestjs/jwt';
 
 // Dto
 import { SignInDto } from '../dto/sign-in.dto';
 
-
 // Exceptions
 import { BadRequestException, RequestTimeoutException } from '@nestjs/common';
+
+// Configurations
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from '../config/jwt.config';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(forwardRef(() => UserService))
     private userService: UserService,
-
     @Inject()
     private readonly hashing: Hashing,
-  ) { }
-
+    @Inject()
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
 
   public isAuth(id: number) {
     return true;
@@ -29,14 +34,33 @@ export class AuthService {
 
   public async SignIn(signInDto: SignInDto) {
     const user = await this.userService.findOneByEmail(signInDto.email);
-    const isPasswordValid = await this.hashing.compare(signInDto.password, user.password);
+    const isPasswordValid = await this.hashing.compare(
+      signInDto.password,
+      user.password,
+    );
 
     if (!isPasswordValid) {
       throw new BadRequestException('Invalid password', {
         description: 'The password you entered is incorrect',
       });
     }
-    delete user.password;
-    return user;
+
+    const token = await this.jwtService.signAsync(
+      {
+        // payload
+        id: user.id,
+        email: user.email,
+      },
+      {
+        // options
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.expiresIn,
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+      },
+    );
+    return {
+      token,
+    };
   }
 }
